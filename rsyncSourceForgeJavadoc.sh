ef3f1c1e04349bec8e1fbb7bca664584ab967a30
eldurloki <eldurloki@users.sourceforge.net>
2013-04-13 18:14:00 +0200

    add updater for sourceforge files

---
+++ rsyncSourceForgeJavadoc.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -eu
+cd target
+upload=./../rsyncSourceForge.sh
+
+remoteWebPath=/home/project-web/jwbf/htdocs
+remoteDocs=$remoteWebPath/doc/
+# remoteTests=$remoteWebPath/tests/
+$upload apidocs/ $remoteDocs --delete
+
+
+
+
---
+++ rsyncSourceForgeSnapshots.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -eu
+upload=./../rsyncSourceForge.sh
+cd target
+incl=include-rsync.tmp
+readme=README.md
+cat > $readme << EOF
+# README
+ 
+- - - 
+* **Files in this folder are NOT necessary the latest snapshot**
+* check also <http://jwbf.sf.net> for snapshots and releases
+- - -
+The files in this folder contains all libraries for useing and developing jwbf. 
+All runtime and test related dependecies are included.
+
+EOF
+rm -f $incl
+echo $readme >> $incl
+echo "jwbf*SNAPSHOT*.tar.gz" >> $incl
+$upload . /home/frs/project/jwbf/snapshots/ --include-from=$incl --exclude=* --delete-after
+
