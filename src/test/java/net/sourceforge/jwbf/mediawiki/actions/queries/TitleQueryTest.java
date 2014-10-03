0fd6f5bf68bf63bba28fb08fa3f4ad662d54d1c0
eldurloki <eldurloki@users.sourceforge.net>
2014-10-03 12:32:12 +0200

    Added test for deprecated TitleQuery

---
+++ src/test/java/net/sourceforge/jwbf/mediawiki/actions/queries/TitleQueryTest.java
@@ -0,0 +1,42 @@
+package net.sourceforge.jwbf.mediawiki.actions.queries;
+
+import static org.mockito.Mockito.mock;
+
+import java.util.Iterator;
+
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableList;
+import net.sourceforge.jwbf.core.actions.util.HttpAction;
+import net.sourceforge.jwbf.mediawiki.bots.MediaWikiBot;
+import org.junit.Test;
+
+public class TitleQueryTest {
+
+  @Test
+  public void testInit() {
+    MediaWikiBot bot = mock(MediaWikiBot.class);
+    new TitleQuery<String>(bot) {
+
+      @Override
+      protected Iterator<String> copy() {
+        return ImmutableList.<String>of().iterator();
+      }
+
+      @Override
+      protected HttpAction prepareCollection() {
+        return null;
+      }
+
+      @Override
+      protected ImmutableList<String> parseArticleTitles(String s) {
+        return ImmutableList.of();
+      }
+
+      @Override
+      protected Optional<String> parseHasMore(String s) {
+        return Optional.absent();
+      }
+    };
+  }
+
+}
