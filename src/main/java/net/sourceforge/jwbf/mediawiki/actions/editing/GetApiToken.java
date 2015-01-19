4d1dba5fd11d4d49940915cd0e446d9b16a170b0
meradi <rabahmeradi@gmail.com>
2015-01-19 22:35:10 +0100

    Use json format for GetApiToken

--- src/main/java/net/sourceforge/jwbf/mediawiki/actions/editing/GetApiToken.java
+++ src/main/java/net/sourceforge/jwbf/mediawiki/actions/editing/GetApiToken.java
@@ -1,22 +1,23 @@
 package net.sourceforge.jwbf.mediawiki.actions.editing;
 
+import java.util.Iterator;
+
 import javax.annotation.Nonnull;
 
-import com.google.common.base.Function;
-import com.google.common.base.Optional;
-import com.google.common.collect.ImmutableMap;
 import net.sourceforge.jwbf.core.Optionals;
 import net.sourceforge.jwbf.core.actions.Get;
 import net.sourceforge.jwbf.core.actions.ParamTuple;
 import net.sourceforge.jwbf.core.actions.util.HttpAction;
-import net.sourceforge.jwbf.core.internal.NonnullFunction;
-import net.sourceforge.jwbf.mapper.XmlConverter;
-import net.sourceforge.jwbf.mapper.XmlElement;
+import net.sourceforge.jwbf.mapper.JsonMapper;
 import net.sourceforge.jwbf.mediawiki.ApiRequestBuilder;
 import net.sourceforge.jwbf.mediawiki.MediaWiki;
 import net.sourceforge.jwbf.mediawiki.actions.util.DequeMWAction;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.base.Optional;
 
 /**
  * This class get the token for some actions like delete or edit.
@@ -27,140 +28,145 @@ import org.slf4j.LoggerFactory;
  */
 public class GetApiToken extends DequeMWAction<GetApiToken.TokenResponse> {
 
-  @Override
-  public TokenResponse get() {
-    return new TokenResponse() {
+    private JsonMapper mapper = new JsonMapper();
 
-      /**
-       * Returns the requested urlEncodedToken after parsing the result from MediaWiki.
-       *
-       * @return the requested urlEncodedToken
-       * @deprecated
-       */
-      @Nonnull
-      @Override
-      @Deprecated
-      public ParamTuple<String> urlEncodedToken() {
-        return new ParamTuple<>("token", MediaWiki.urlEncode(getToken()));
-      }
+    @Override
+    public TokenResponse get() {
+        return new TokenResponse() {
 
-      private String getToken() {
-        return Optionals.getOrThrow(GetApiToken.this.token, "The argument 'token' is missing");
-      }
+            /**
+             * Returns the requested urlEncodedToken after parsing the result from MediaWiki.
+             *
+             * @return the requested urlEncodedToken
+             * @deprecated
+             */
+            @Nonnull
+            @Override
+            @Deprecated
+            public ParamTuple<String> urlEncodedToken() {
+                return new ParamTuple<>("token", MediaWiki.urlEncode(getToken()));
+            }
 
-      @Nonnull
-      @Override
-      public ParamTuple<String> token() {
-        return new ParamTuple<>("token", getToken());
-      }
-    };
-  }
+            private String getToken() {
+                return Optionals.getOrThrow(GetApiToken.this.token,
+                        "The argument 'token' is missing");
+            }
 
-  public interface TokenResponse {
+            @Nonnull
+            @Override
+            public ParamTuple<String> token() {
+                return new ParamTuple<>("token", getToken());
+            }
+        };
+    }
+
+    public interface TokenResponse {
+
+        /**
+         * @deprecated prefer {@link #token()} with postPram.
+         */
+        @Deprecated
+        ParamTuple<String> urlEncodedToken();
+
+        ParamTuple<String> token();
+    }
+
+    private static final Logger log = LoggerFactory.getLogger(GetApiToken.class);
 
     /**
-     * @deprecated prefer {@link #token()} with postPram.
+     * Types that need a urlEncodedToken. See API field intoken.
      */
-    @Deprecated
-    ParamTuple<String> urlEncodedToken();
+    // TODO this does not feel the elegant way.
+    // Probably put complete request URIs into this enum objects
+    // to support different URIs for different actions.
+    public enum Intoken {
+        DELETE("delete", "deletetoken"), EDIT("edit", "edittoken"), MOVE("move", "movetoken"),
+        PROTECT("protect", "protecttoken"), EMAIL("email", "emailtoken"), BLOCK("block",
+                "blocktoken"), UNBLOCK("unblock", "unblocktoken"), IMPORT("import", "IMPORT");
 
-    ParamTuple<String> token();
-  }
+        private String requestName;
+        private String responseName;
 
-  private static final Logger log = LoggerFactory.getLogger(GetApiToken.class);
-
-  /**
-   * Types that need a urlEncodedToken. See API field intoken.
-   */
-  // TODO this does not feel the elegant way.
-  // Probably put complete request URIs into this enum objects
-  // to support different URIs for different actions.
-  public enum Intoken {
-    DELETE, EDIT, MOVE, PROTECT, EMAIL, BLOCK, UNBLOCK, IMPORT
-  }
-
-  private static final ImmutableMap<Intoken, Function<XmlElement, String>> TOKEN_FUNCTIONS =
-      ImmutableMap.<Intoken, Function<XmlElement, String>>builder() //
-          .put(Intoken.DELETE, tokenFunctionOf("deletetoken")) //
-          .put(Intoken.EDIT, tokenFunctionOf("edittoken")) //
-          .put(Intoken.MOVE, tokenFunctionOf("movetoken")) //
-          .put(Intoken.PROTECT, tokenFunctionOf("protecttoken")) //
-          .put(Intoken.EMAIL, tokenFunctionOf("emailtoken")) //
-          .put(Intoken.BLOCK, tokenFunctionOf("blocktoken")) //
-          .put(Intoken.UNBLOCK, tokenFunctionOf("unblocktoken")) //
-          .put(Intoken.IMPORT, tokenFunctionOf("IMPORT")) //
-          .build();
-
-  private Optional<String> token = Optional.absent();
-
-  private final Intoken intoken;
-
-  private final HttpAction msg;
-
-  /**
-   * Constructs a new <code>GetToken</code> action.
-   *
-   * @param intoken type to get the token for
-   * @param title   title of the article to generate the token for
-   */
-  public GetApiToken(Intoken intoken, String title) {
-    super(generateTokenRequest(intoken, title));
-    this.intoken = intoken;
-    msg = actions.getFirst(); // XXX realy nesessary?
-
-  }
-
-  /**
-   * Generates the next MediaWiki API urlEncodedToken and adds it to <code>msgs</code>.
-   *
-   * @param intoken type to get the urlEncodedToken for
-   * @param title   title of the article to generate the urlEncodedToken for
-   */
-  private static Get generateTokenRequest(Intoken intoken, String title) {
-    return new ApiRequestBuilder() //
-        .action("query") //
-        .formatXml() //
-        .param("prop", "info") //
-        .param("intoken", intoken.toString().toLowerCase()) //
-        .param("titles", MediaWiki.urlEncode(title)) //
-        .buildGet();
-
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public void processReturningText(String s, HttpAction hm) {
-    if (hm.getRequest().equals(msg.getRequest())) {
-      log.debug("Got returning text: \"{}\"", s);
-      try {
-        Optional<XmlElement> elem = XmlConverter.getChildOpt(s, "query", "pages", "page");
-        token = elem.transform(TOKEN_FUNCTIONS.get(intoken));
-        // TODO check intoken from tokenfunc for null
-
-        log.debug("urlEncodedToken = {} for: {}", token, msg.getRequest());
-        // TODO check catch
-      } catch (IllegalArgumentException e) {
-        if (s.startsWith("unknown_action:")) {
-          log.error("Adding '$wgEnableWriteAPI = true;' "
-              + "to your MediaWiki's LocalSettings.php might remove this problem.", e);
-        } else {
-          log.error(e.getMessage(), e);
+        Intoken(String requestName, String responseName) {
+            this.requestName = requestName;
+            this.responseName = responseName;
         }
-      }
+
+        public String getRequestName() {
+            return requestName;
+        }
+
+        public String getResponseName() {
+            return responseName;
+        }
     }
-  }
 
-  private static Function<XmlElement, String> tokenFunctionOf(final String key) {
-    return new NonnullFunction<XmlElement, String>() {
+    private Optional<String> token = Optional.absent();
 
-      @Nonnull
-      @Override
-      public String applyNonnull(@Nonnull XmlElement xmlElement) {
-        return xmlElement.getAttributeValueNonNull(key);
-      }
-    };
-  }
+    private final Intoken intoken;
+
+    private final HttpAction msg;
+
+    /**
+     * Constructs a new <code>GetToken</code> action.
+     *
+     * @param intoken
+     *            type to get the token for
+     * @param title
+     *            title of the article to generate the token for
+     */
+    public GetApiToken(Intoken intoken, String title) {
+        super(generateTokenRequest(intoken, title));
+        this.intoken = intoken;
+        msg = actions.getFirst(); // XXX realy nesessary?
+
+    }
+
+    /**
+     * Generates the next MediaWiki API urlEncodedToken and adds it to <code>msgs</code>.
+     *
+     * @param intoken
+     *            type to get the urlEncodedToken for
+     * @param title
+     *            title of the article to generate the urlEncodedToken for
+     */
+    private static Get generateTokenRequest(Intoken intoken, String title) {
+        return new ApiRequestBuilder() //
+                .action("query") //
+                .formatJson() //
+                .param("prop", "info") //
+                .param("intoken", intoken.getRequestName()) //
+                .param("titles", MediaWiki.urlEncode(title)) //
+                .buildGet();
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void processReturningText(String s, HttpAction hm) {
+        if (hm.getRequest().equals(msg.getRequest())) {
+            log.debug("Got returning text: \"{}\"", s);
+            try {
+                JsonNode node = mapper.toJsonNode(s).path("query").path("pages");
+                String fieldName = node.fieldNames().next();
+                node = node.get(fieldName)
+                        .get(intoken.getResponseName());
+                token = Optional.of(node.asText());
+                // TODO check intoken from tokenfunc for null
+
+                log.debug("urlEncodedToken = {} for: {}", token, msg.getRequest());
+                // TODO check catch
+            } catch (IllegalArgumentException e) {
+                if (s.startsWith("unknown_action:")) {
+                    log.error("Adding '$wgEnableWriteAPI = true;' " +
+                            "to your MediaWiki's LocalSettings.php might remove this problem.", e);
+                } else {
+                    log.error(e.getMessage(), e);
+                }
+            }
+        }
+    }
 
 }
--- src/test/java/net/sourceforge/jwbf/mediawiki/actions/editing/GetApiTokenTest.java
+++ src/test/java/net/sourceforge/jwbf/mediawiki/actions/editing/GetApiTokenTest.java
@@ -4,81 +4,72 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-
 import net.sourceforge.jwbf.TestHelper;
 import net.sourceforge.jwbf.core.actions.ParamTuple;
 import net.sourceforge.jwbf.core.actions.util.HttpAction;
 import net.sourceforge.jwbf.mediawiki.actions.editing.GetApiToken.Intoken;
+
 import org.junit.Test;
 
 public class GetApiTokenTest {
 
-  private GetApiToken testee;
+    private GetApiToken testee;
 
-  @Test
-  public void testProcessReturningText() {
-    // GIVEN
-    testee = new GetApiToken(Intoken.EDIT, "test");
-    String xml = TestHelper.anyWikiResponse("intoken.xml");
+    @Test
+    public void testProcessReturningText() {
+        // GIVEN
+        testee = new GetApiToken(Intoken.EDIT, "test");
+        String xml = TestHelper.anyWikiResponse("intoken.json");
 
-    // WHEN
-    testee.processReturningText(xml, testee.popAction());
+        // WHEN
+        testee.processReturningText(xml, testee.popAction());
 
-    // THEN
-    ParamTuple<String> encodedToken =
-        new ParamTuple("token", "e0691d5329779f0c01b1b286cd44a278%2B%5C");
-    assertEquals(encodedToken, testee.get().urlEncodedToken());
-    ParamTuple<String> token = new ParamTuple("token", "e0691d5329779f0c01b1b286cd44a278+\\");
-    assertEquals(token, testee.get().token());
-  }
-
-  @Test
-  public void testProcessReturningText_requestMissmatch() {
-    // GIVEN
-    testee = new GetApiToken(Intoken.DELETE, "test");
-    String xml = TestHelper.anyWikiResponse("intoken.xml");
-
-    // WHEN
-    try {
-      testee.processReturningText(xml, testee.popAction());
-      fail();
-    } catch (NullPointerException e) {
-      // THEN
-      assertEquals("attribute value for key: deletetoken must not be null", e.getMessage());
+        // THEN
+        ParamTuple<String> encodedToken = new ParamTuple<String>("token",
+                "e0691d5329779f0c01b1b286cd44a278%2B%5C");
+        assertEquals(encodedToken, testee.get().urlEncodedToken());
+        ParamTuple<String> token = new ParamTuple<String>("token",
+                "e0691d5329779f0c01b1b286cd44a278+\\");
+        assertEquals(token, testee.get().token());
     }
 
-  }
-
-  @Test
-  public void testGetToken() {
-    // GIVEN
-    testee = new GetApiToken(Intoken.BLOCK, "test");
-
-    try {
-      // WHEN
-      testee.get().token();
-      fail();
-    } catch (IllegalArgumentException e) {
-      // THEN
-      assertEquals("The argument 'token' is missing", e.getMessage());
+    @Test(expected = NullPointerException.class)
+    public void testProcessReturningText_requestMissmatch() {
+        testee = new GetApiToken(Intoken.DELETE, "test");
+        String json = TestHelper.anyWikiResponse("intoken.json");
+        testee.processReturningText(json, testee.popAction());
     }
-  }
 
-  @Test
-  public void testGetNextMessage() {
-    // GIVEN
-    String title = "test";
-    testee = new GetApiToken(Intoken.MOVE, title);
+    @Test
+    public void testGetToken() {
+        // GIVEN
+        testee = new GetApiToken(Intoken.BLOCK, "test");
 
-    // WHEN
-    assertTrue(testee.hasMoreActions());
-    HttpAction first = testee.popAction();
+        try {
+            // WHEN
+            testee.get().token();
+            fail();
+        } catch (IllegalArgumentException e) {
+            // THEN
+            assertEquals("The argument 'token' is missing", e.getMessage());
+        }
+    }
 
-    // THEN
-    assertFalse(testee.hasMoreActions());
-    assertEquals("/api.php?action=query&format=xml&intoken=move&prop=info&titles=" + title,
-        first.getRequest());
+    @Test
+    public void testGetNextMessage() {
+        // GIVEN
+        String title = "test";
+        testee = new GetApiToken(Intoken.MOVE, title);
 
-  }
+        // WHEN
+        assertTrue(testee.hasMoreActions());
+        HttpAction first = testee.popAction();
+
+        // THEN
+        assertFalse(testee.hasMoreActions());
+        assertEquals("/api.php?action=query&format=json&intoken=move&prop=info&titles=" + title,
+                first.getRequest());
+
+    }
 
 }
