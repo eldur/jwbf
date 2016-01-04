8dc4008ee75cd0b9a6829e2836ff7402f4c11c4c
Marco Ammon <ammon.marco@t-online.de>
2016-01-05 00:35:35 +0100

    Now properly loading additional results

--- src/main/java/net/sourceforge/jwbf/mediawiki/actions/queries/ReviewedPagesTitles.java
+++ src/main/java/net/sourceforge/jwbf/mediawiki/actions/queries/ReviewedPagesTitles.java
@@ -15,7 +15,6 @@
  */
 package net.sourceforge.jwbf.mediawiki.actions.queries;
 
-
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
@@ -39,118 +38,114 @@ import org.slf4j.LoggerFactory;
  */
 public class ReviewedPagesTitles extends BaseQuery<String> {
 
-  private static final Logger log = LoggerFactory.getLogger(net.sourceforge.jwbf.mediawiki.actions.queries.RecentchangeTitles.class);
+    private static final Logger log = LoggerFactory.getLogger(net.sourceforge.jwbf.mediawiki.actions.queries.RecentchangeTitles.class);
+    /**
+     * value for the rplimit-parameter.
+     */
+    private static final int LIMIT = 50;
+    private final MediaWikiBot bot;
+    private final int[] namespaces;
 
-  /**
-   * value for the rplimit-parameter.
-   */
-  private static final int LIMIT = 50;
+    /**
+     * generates the next MediaWiki-request (GetMethod) and adds it to msgs.
+     *
+     * @param namespace the namespace(s) that will be searched for links, as a
+     * string of numbers separated by '|'; if null, this parameter is omitted
+     * @param rpstart Start listing at this page id
+     *
+     * @param rpend Stop listing at this page id
+     */
+    private HttpAction generateRequest(int[] namespace, String rpstart, String rpend) {
 
-  private final MediaWikiBot bot;
+        RequestBuilder requestBuilder = new ApiRequestBuilder() //
+                .action("query") //
+                .formatXml() //
+                .param("list", "reviewedpages") //
+                .param("rplimit", LIMIT) //
+                ;
+        if (namespace != null) {
+            requestBuilder.param("rpnamespace", MediaWiki.urlEncode(MWAction.createNsString(namespace)));
+        }
+        if (rpstart.length() > 0) {
+            requestBuilder.param("rpstart", rpstart);
+        }
+        if (rpend.length() > 0) {
+            requestBuilder.param("rpend", rpend);
+        }
 
-  private final int[] namespaces;
-
-  /**
-   * generates the next MediaWiki-request (GetMethod) and adds it to msgs.
-   *
-   * @param namespace the namespace(s) that will be searched for links, as a string of numbers
-   *                  separated by '|'; if null, this parameter is omitted
-   * @param rpstart   Start listing at this page title
-   * 
-   * @param rpend     Stop listing at this page title
-   */
-  private HttpAction generateRequest(int[] namespace, String rpstart, String rpend) {
-
-    RequestBuilder requestBuilder = new ApiRequestBuilder() //
-        .action("query") //
-        .formatXml() //
-        .param("list", "reviewedpages") //
-        .param("rplimit", LIMIT) //
-        ;
-    if (namespace != null) {
-      requestBuilder.param("rpnamespace", MediaWiki.urlEncode(MWAction.createNsString(namespace)));
-    }
-    if (rpstart.length() > 0) {
-      requestBuilder.param("rpstart", rpstart);
-    }
-    if (rpend.length() > 0) {
-      requestBuilder.param("rpend", rpend);
-    }
-
-    return requestBuilder.buildGet();
-
-  }
-  
-  private HttpAction generateRequest(int[] namespace, String orstart){
-      return generateRequest(namespace, orstart, "");
-  }
-  
-  private HttpAction generateRequest(int[] namespace) {
-    return generateRequest(namespace, "", "");
-  }
-
-  /**
-   *
-   */
-  public ReviewedPagesTitles(MediaWikiBot bot, int... ns) {
-    super(bot);
-    namespaces = ns;
-    this.bot = bot;
-
-  }
-
-  /**
-   *
-   */
-  public ReviewedPagesTitles(MediaWikiBot bot) {
-    this(bot, MediaWiki.NS_ALL);
-  }
-
-  /**
-   * picks the article name from a MediaWiki api response.
-   *
-   * @param s text for parsing
-   */
-  @Override
-  protected ImmutableList<String> parseElements(String s) {
-    XmlElement root = XmlConverter.getRootElement(s);
-    List<String> titleCollection = Lists.newArrayList();
-    findContent(root, titleCollection);
-    return ImmutableList.copyOf(titleCollection);
-
-  }
-
-  private void findContent(final XmlElement root, List<String> titleCollection) {
-
-    for (XmlElement xmlElement : root.getChildren()) {
-      if (xmlElement.getQualifiedName().equalsIgnoreCase("p")) {
-        titleCollection.add(MediaWiki.htmlUnescape(xmlElement.getAttributeValue("title")));
-        setNextPageInfo(xmlElement.getAttributeValue("title"));
-      } else {
-        findContent(xmlElement, titleCollection);
-      }
+        return requestBuilder.buildGet();
 
     }
-  }
 
-  @Override
-  protected HttpAction prepareNextRequest() {
-    if (hasNextPageInfo()) {
-      return generateRequest(namespaces, getNextPageInfo());
-    } else {
-      return generateRequest(namespaces);
+    private HttpAction generateRequest(int[] namespace, String rpstart) {
+        return generateRequest(namespace, rpstart, "");
     }
 
-  }
+    private HttpAction generateRequest(int[] namespace) {
+        return generateRequest(namespace, "", "");
+    }
 
-  @Override
-  protected Iterator<String> copy() {
-    return new net.sourceforge.jwbf.mediawiki.actions.queries.OldreviewedPagesTitles(bot, namespaces);
-  }
+    /**
+     *
+     */
+    public ReviewedPagesTitles(MediaWikiBot bot, int... ns) {
+        super(bot);
+        namespaces = ns;
+        this.bot = bot;
 
-  @Override
-  protected Optional<String> parseHasMore(String s) {
-    return Optional.absent();
-  }
+    }
 
+    /**
+     *
+     */
+    public ReviewedPagesTitles(MediaWikiBot bot) {
+        this(bot, MediaWiki.NS_ALL);
+    }
+
+    /**
+     * picks the article name from a MediaWiki api response.
+     *
+     * @param s text for parsing
+     */
+    @Override
+    protected ImmutableList<String> parseElements(String s) {
+        XmlElement root = XmlConverter.getRootElement(s);
+        List<String> titleCollection = Lists.newArrayList();
+        findContent(root, titleCollection);
+        return ImmutableList.copyOf(titleCollection);
+
+    }
+
+    private void findContent(final XmlElement root, List<String> titleCollection) {
+
+        for (XmlElement xmlElement : root.getChildren()) {
+            if (xmlElement.getQualifiedName().equalsIgnoreCase("p")) {
+                titleCollection.add(MediaWiki.htmlUnescape(xmlElement.getAttributeValue("title")));
+            } else {
+                findContent(xmlElement, titleCollection);
+            }
+
+        }
+    }
+
+    @Override
+    protected HttpAction prepareNextRequest() {
+        Optional<String> rpcontinue = nextPageInfoOpt();
+        if (rpcontinue.isPresent()) {
+            return generateRequest(namespaces, rpcontinue.get());
+        } else {
+            return generateRequest(namespaces);
+        }
+
+    }
+
+    @Override
+    protected Iterator<String> copy() {
+        return new net.sourceforge.jwbf.mediawiki.actions.queries.OldreviewedPagesTitles(bot, namespaces);
+    }
+
+    @Override
+    protected Optional<String> parseHasMore(final String xml) {
+        return parseXmlHasMore(xml, "reviewedpages", "rpstart", "rpstart");
+    }
 }
--- src/main/java/net/sourceforge/jwbf/mediawiki/actions/queries/UnreviewedPagesTitles.java
+++ src/main/java/net/sourceforge/jwbf/mediawiki/actions/queries/UnreviewedPagesTitles.java
@@ -15,9 +15,6 @@
  */
 package net.sourceforge.jwbf.mediawiki.actions.queries;
 
-  
-
-
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
@@ -41,121 +38,115 @@ import org.slf4j.LoggerFactory;
  */
 public class UnreviewedPagesTitles extends BaseQuery<String> {
 
-  private static final Logger log = LoggerFactory.getLogger(net.sourceforge.jwbf.mediawiki.actions.queries.UnreviewedPagesTitles.class);
+    private static final Logger log = LoggerFactory.getLogger(net.sourceforge.jwbf.mediawiki.actions.queries.UnreviewedPagesTitles.class);
+    /**
+     * value for the urlimit-parameter.
+     *
+     */
+    private static int LIMIT;
+    private final MediaWikiBot bot;
+    private final int[] namespaces;
 
-  /**
-   * value for the urlimit-parameter. 
-   *
-   */
-  private static int LIMIT;
+    /**
+     * generates the next MediaWiki-request (GetMethod) and adds it to msgs.
+     *
+     * @param namespace the namespace(s) that will be searched for links, as a
+     * string of numbers separated by '|'; if null, this parameter is omitted
+     * @param urstart Start listing at this page title
+     *
+     * @param urend Stop listing at this page title
+     *
+     */
+    private HttpAction generateRequest(int[] namespace, String urstart, String urend) {
 
-  private final MediaWikiBot bot;
+        RequestBuilder requestBuilder = new ApiRequestBuilder() //
+                .action("query") //
+                .formatXml() //
+                .param("list", "unreviewedpages") //
+                .param("urlimit", LIMIT) //
+                ;
+        if (namespace != null) {
+            requestBuilder.param("urnamespace", MediaWiki.urlEncode(MWAction.createNsString(namespace)));
+        }
+        if (urstart.length() > 0) {
+            requestBuilder.param("urstart", urstart);
+        }
+        if (urend.length() > 0) {
+            requestBuilder.param("urend", urend);
+        }
 
-  private final int[] namespaces;
-
-  /**
-   * generates the next MediaWiki-request (GetMethod) and adds it to msgs.
-   *
-   * @param namespace the namespace(s) that will be searched for links, as a string of numbers
-   *                  separated by '|'; if null, this parameter is omitted
-   * @param urstart   Start listing at this page title
-   * 
-   * @param urend     Stop listing at this page title
-   * 
-   */
-  
-  private HttpAction generateRequest(int[] namespace, String urstart, String urend) {
-    
-    RequestBuilder requestBuilder = new ApiRequestBuilder() //
-        .action("query") //
-        .formatXml() //
-        .param("list", "unreviewedpages") //
-        .param("urlimit", LIMIT) //
-        ;
-    if (namespace != null) {
-      requestBuilder.param("urnamespace", MediaWiki.urlEncode(MWAction.createNsString(namespace)));
-    }
-    if (urstart.length() > 0) {
-      requestBuilder.param("urstart", urstart);
-    }
-    if(urend.length() > 0){
-        requestBuilder.param("urend", urend);
-    }
-
-    return requestBuilder.buildGet();
-
-  }
-  private HttpAction generateRequest(int[] namespace, String urstart) {
-      return generateRequest(namespace, urstart, "");
-  }
-  
-  private HttpAction generateRequest(int[] namespace) {
-    return generateRequest(namespace, "", "");
-  }
-
-  /**
-   *
-   */
-  public UnreviewedPagesTitles(MediaWikiBot bot, int... ns) {
-    super(bot);
-    namespaces = ns;
-    this.bot = bot;
-  }
-
-  /**
-   *
-   */
-  public UnreviewedPagesTitles(MediaWikiBot bot) {
-    this(bot, MediaWiki.NS_ALL);
-  }
-
-  /**
-   * picks the article name from a MediaWiki api response.
-   *
-   * @param s text for parsing
-   */
-  @Override
-  protected ImmutableList<String> parseElements(String s) {
-    XmlElement root = XmlConverter.getRootElement(s);
-    List<String> titleCollection = Lists.newArrayList();
-    findContent(root, titleCollection);
-    return ImmutableList.copyOf(titleCollection);
-
-  }
-
-  private void findContent(final XmlElement root, List<String> titleCollection) {
-
-    for (XmlElement xmlElement : root.getChildren()) {
-      if (xmlElement.getQualifiedName().equalsIgnoreCase("p")) {
-        titleCollection.add(MediaWiki.htmlUnescape(xmlElement.getAttributeValue("title")));
-        //Not sure about this, RecentChanges doesn't have timestamp either
-        setNextPageInfo(xmlElement.getAttributeValue("title"));
-      } else {
-        findContent(xmlElement, titleCollection);
-      }
+        return requestBuilder.buildGet();
 
     }
-  }
 
-  @Override
-  protected HttpAction prepareNextRequest() {
-    if (hasNextPageInfo()) {
-      return generateRequest(namespaces, getNextPageInfo());
-    } else {
-      return generateRequest(namespaces);
+    private HttpAction generateRequest(int[] namespace, String urstart) {
+        return generateRequest(namespace, urstart, "");
     }
 
-  }
+    private HttpAction generateRequest(int[] namespace) {
+        return generateRequest(namespace, "", "");
+    }
 
-  @Override
-  protected Iterator<String> copy() {
-    return new net.sourceforge.jwbf.mediawiki.actions.queries.UnreviewedPagesTitles(bot, namespaces);
-  }
+    /**
+     *
+     */
+    public UnreviewedPagesTitles(MediaWikiBot bot, int... ns) {
+        super(bot);
+        namespaces = ns;
+        this.bot = bot;
+    }
 
-  @Override
-  protected Optional<String> parseHasMore(String s) {
-    return Optional.absent();
-  }
+    /**
+     *
+     */
+    public UnreviewedPagesTitles(MediaWikiBot bot) {
+        this(bot, MediaWiki.NS_ALL);
+    }
 
+    /**
+     * picks the article name from a MediaWiki api response.
+     *
+     * @param s text for parsing
+     */
+    @Override
+    protected ImmutableList<String> parseElements(String s) {
+        XmlElement root = XmlConverter.getRootElement(s);
+        List<String> titleCollection = Lists.newArrayList();
+        findContent(root, titleCollection);
+        return ImmutableList.copyOf(titleCollection);
+
+    }
+
+    private void findContent(final XmlElement root, List<String> titleCollection) {
+
+        for (XmlElement xmlElement : root.getChildren()) {
+            if (xmlElement.getQualifiedName().equalsIgnoreCase("p")) {
+                titleCollection.add(MediaWiki.htmlUnescape(xmlElement.getAttributeValue("title")));
+            } else {
+                findContent(xmlElement, titleCollection);
+            }
+
+        }
+    }
+
+    @Override
+    protected HttpAction prepareNextRequest() {
+        Optional<String> urcontinue = nextPageInfoOpt();
+        if (urcontinue.isPresent()) {
+            return generateRequest(namespaces, urcontinue.get());
+        } else {
+            return generateRequest(namespaces);
+        }
+
+    }
+
+    @Override
+    protected Iterator<String> copy() {
+        return new net.sourceforge.jwbf.mediawiki.actions.queries.UnreviewedPagesTitles(bot, namespaces);
+    }
+
+    @Override
+    protected Optional<String> parseHasMore(final String xml) {
+        return parseXmlHasMore(xml, "unreviewedpages", "urstart", "urstart");
+    }
 }
-
