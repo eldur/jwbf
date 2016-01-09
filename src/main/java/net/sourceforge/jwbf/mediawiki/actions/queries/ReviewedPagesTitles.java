f31c99b4759f3cde004e5901097e927934d81307
Marco Ammon <ammon.marco@t-online.de>
2016-01-09 15:53:35 +0100

    Replaced wrong call (tests are now passing locally)

--- src/main/java/net/sourceforge/jwbf/mediawiki/actions/queries/ReviewedPagesTitles.java
+++ src/main/java/net/sourceforge/jwbf/mediawiki/actions/queries/ReviewedPagesTitles.java
@@ -142,7 +142,7 @@ public class ReviewedPagesTitles extends BaseQuery<String> {
 
     @Override
     protected Iterator<String> copy() {
-        return new OldreviewedPagesTitles(bot, namespaces);
+        return new ReviewedPagesTitles(bot, namespaces);
     }
 
     @Override
